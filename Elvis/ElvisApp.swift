d4b3591384edcf45e2be182f9e358dae3cb71e75
Nick Collins <necollins03@gmail.com>
2026-01-19 12:10:03 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Nick Collins on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
